2f0dd6dc0e2b85eab7a6c78ec81c41c5d4af2bc9
User <user@Mac.local>
2018-05-13 20:50:34 +1000

    added index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+function fib(n) {
+    if(n< 2) {
+      return n;
+    }
+    return fib(n-1) + fib(n-2);
+  }
+
+
+// function fib(n) {
+//   var result = [0, 1];
+
+//   for(var i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//     result.push(a + b);
+//   }
+
+//    return result[n];
+// }
